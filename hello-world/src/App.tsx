05122a0e250c846c58787250d156be90dd282ec8
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 12:44:02 +0900

    add semantic-uiを入れて関数型コンポーネントをためす
    レンダリングするReactElementをreturnする

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,60 +1,46 @@
-import React, { FC } from 'react';
-import Greets from 'components/Greets';
-import TextInput from 'components/TextInput';
-import logo from './logo.svg';
+import { VFC } from 'react';
+import CharacterList, { Character } from 'components/CharacterList';
 import './App.css';
 
-const App: FC = () => {
-  const name = 'Patty';
-  const grret = (displayName: string) => (
-    <p>
-      Hello,
-      {displayName || 'Guest'}
-    </p>
-  );
-  const testN = Math.floor(Math.random() * 10);
-  const threshold = 5;
+const App: VFC = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '桜木花道',
+      grade: 1,
+      height: 189.2,
+    },
+    {
+      id: 2,
+      name: '流川 楓',
+      grade: 1,
+      height: 187,
+    },
+    {
+      id: 3,
+      name: '宮城リョータ',
+      grade: 2,
+      height: 168,
+    },
+    {
+      id: 4,
+      name: '三井 寿',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: '赤木剛憲',
+      grade: 3,
+      height: 197,
+    },
+  ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {grret(name)}
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <p>
-          Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          { testN > threshold && (
-          <p>
-            testN is larger than
-            { threshold }
-          </p>
-          )}
-          <p>
-            n is
-            { testN % 2 === 0 ? 'even' : 'odd'}
-          </p>
-        </div>
+    <div className="container">
+      <header>
+        <h1>『SLAM DUNK』登場人物</h1>
       </header>
-      <Greets name="hogege" times={3} />
-      <TextInput />
+      <CharacterList school="湘北高校" characters={characters} />
     </div>
   );
 };
