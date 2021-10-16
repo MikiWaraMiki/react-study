d48f6b93e6f41cf14ba2dbb5346b1f9a6b8d333a
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 15:50:16 +0900

    add link

--- hello-world/src/components/pages/404.tsx
+++ hello-world/src/components/pages/404.tsx
@@ -1,9 +1,20 @@
 import { VFC } from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound: VFC = () => (
-  <>
+  <div>
     <h1>404</h1>
-  </>
+    <Link
+      to={{
+        pathname: '/',
+        search: '?from=here',
+        hash: '#subject',
+        state: { secretCode: 'hogehoge' },
+      }}
+    >
+      お問い合わせ
+    </Link>
+  </div>
 );
 
 export default NotFound;
