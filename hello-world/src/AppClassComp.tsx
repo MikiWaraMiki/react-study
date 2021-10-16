be273b696ddb07b2ec660c1d736f6b358ed70b7c
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 13:24:13 +0900

    add callback
    callbackを利用したタイマー
    componentDidMount, componentDidUpdate, componentWillMountを利用している

--- hello-world/src/AppClassComp.tsx
+++ hello-world/src/AppClassComp.tsx
@@ -1,27 +1,43 @@
-import {Component, ReactElement, SyntheticEvent} from "react";
+import {Component, ReactElement} from "react";
 import './App.css'
-import {Button, Card, Statistic} from "semantic-ui-react";
+import {Button, Card, Statistic, Icon} from "semantic-ui-react";
 
-type State = { count: number }
+const LIMIT = 60
+type State = { timeLeft: number }
 
 class AppClassComp extends Component<unknown, State> {
+  // eslint-disable-next-line react/sort-comp
+  timerId: NodeJS.Timer | null = null
+
   constructor(props: unknown) {
     super(props);
-    this.state = { count: 0 }
+    this.state = { timeLeft: LIMIT }
   }
 
-  reset = (e: SyntheticEvent): void  => {
-    e.preventDefault()
-    this.setState({count: 0})
+  // コンポーネントがマウントされた直後
+  componentDidMount = (): void => {
+    this.timerId = setInterval(this.tick, 1000)
   }
 
-  increment = (e: SyntheticEvent): void  => {
-    e.preventDefault()
-    this.setState((state) => ({count: state.count + 1}))
+  // コンポーネントが再レンダリングされた直後
+  componentDidUpdate = (): void => {
+    const {timeLeft} = this.state
+    if (timeLeft === 0 ) this.reset()
   }
+
+  // コンポーネントがアンマウントされて破棄される直前
+  componentWillUnmount = (): void => {
+    if (this.timerId) clearInterval(this.timerId)
+  }
+
+  tick = (): void =>
+    this.setState((prevState) => ({timeLeft: prevState.timeLeft - 1}))
+
+  reset = (): void =>
+    this.setState({timeLeft: LIMIT} )
 
   render(): ReactElement {
-    const { count } = this.state
+    const { timeLeft } = this.state
 
     return (
       <div className="container">
@@ -30,16 +46,14 @@ class AppClassComp extends Component<unknown, State> {
         </header>
         <Card>
           <Statistic className="number-board">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
+            <Statistic.Label>time</Statistic.Label>
+            <Statistic.Value>{timeLeft}</Statistic.Value>
           </Statistic>
           <Card.Content>
             <div className="ui two buttons">
               <Button color="red" onClick={this.reset}>
+                <Icon name="redo"/>
                 Reset
-              </Button>
-              <Button color="green" onClick={this.increment}>
-                +1
               </Button>
             </div>
           </Card.Content>
