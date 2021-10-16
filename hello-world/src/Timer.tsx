3a0e72e1c305f18fbc1e85ddcbcae9db86bd585c
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 14:47:55 +0900

    add memonize
    メモ化を試す。useMemoで、レンダリングごとに実行される処理を制限できる。

---
+++ hello-world/src/Timer.tsx
@@ -0,0 +1,47 @@
+import './Timer.css';
+import { useCallback, useEffect, useMemo, useState, VFC } from 'react';
+import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
+import { getPrimes } from 'util/math-tool';
+
+type TimerProps = {
+  limit: number;
+};
+
+const Timer: VFC<TimerProps> = ({ limit }) => {
+  const [timeLeft, setTimeLeft] = useState(limit);
+
+  const primes = useMemo(() => getPrimes(limit), [limit]);
+  const reset = useCallback((): void => setTimeLeft(limit), [limit]);
+  const tick = (): void => setTimeLeft((t) => t - 1);
+
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
+
+    return () => clearInterval(timerId);
+  });
+
+  useEffect(() => {
+    if (timeLeft === 0) reset();
+  }, [timeLeft, reset]);
+
+  return (
+    <Card>
+      <Statistic className="number-board">
+        <Statistic.Label>time</Statistic.Label>
+        <Statistic.Value
+          className={primes.includes(timeLeft) ? 'prime-number' : undefined}
+        >
+          {timeLeft}
+        </Statistic.Value>
+      </Statistic>
+      <Card.Content>
+        <Button color="red" fluid onClick={reset}>
+          <Icon name="redo" />
+          reset
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default Timer;
--- hello-world/src/index.tsx
+++ hello-world/src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 // import App from './App';
-import Counter from './Counter';
+// import Counter from './Counter';
+import Timer from './Timer';
 import reportWebVitals from './reportWebVitals';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Timer limit={60} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
