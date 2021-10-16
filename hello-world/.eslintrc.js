95a0dd91e2e6471979b1b5dc8896d91af064b9e9
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 11:52:49 +0900

    add prefer-arrow
    関数定義をアロー関数に統一するためにprefer-arrowを導入した

--- hello-world/.eslintrc.js
+++ hello-world/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     "@typescript-eslint",
     "import",
     "jsx-a11y",
+    'prefer-arrow',
     "react",
     "react-hooks"
   ],
@@ -91,7 +92,15 @@ module.exports = {
         explicitSpread: 'ignore'
       }
     ],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false
+      }
+    ]
   },
   overrides: [
     {
