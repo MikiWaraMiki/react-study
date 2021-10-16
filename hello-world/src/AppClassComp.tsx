3002b3a471c16364adb9f2e1a1b8fbbddb72f237
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 14:00:54 +0900

    add HOCを利用する

--- hello-world/src/AppClassComp.tsx
+++ hello-world/src/AppClassComp.tsx
@@ -1,59 +1,59 @@
-// eslint-disable-next-line react/destructuring-assignment
+/* eslint-disable-next-line react/destructuring-assignment */
 import { Component, ReactElement, VFC } from 'react';
 import './App.css';
-import { Button, Card, Statistic, Icon } from 'semantic-ui-react';
 
-const LIMIT = 60;
-type InjectedProps = {
+type ChildProps = {
   count: number;
   reset: () => void;
   increment: () => void;
 };
-type Props = { max: number };
+
+type Props = {
+  max: number;
+  children: (props: ChildProps) => ReactElement;
+};
+
 type State = { count: number };
 
-const withCounter = (WrappedComponent: VFC<Props & Partial<InjectedProps>>) =>
-  class EnhancedComponent extends Component<Props, State> {
-    constructor(props: Props) {
-      super(props);
-      this.state = { count: 0 };
-    }
+class CounterProvider extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-    reset = (): void => this.setState({ count: 0 });
-    increment = (): void =>
-      this.setState((prevState) => ({ count: prevState.count + 1 }));
+  // eslint-disable-next-line react/sort-comp
+  reset = (): void => this.setState({ count: 0 });
+  increment = (): void =>
+    this.setState((state) => ({ count: state.count + 1 }));
 
-    componentDidUpdate = (): void => {
-      if (this.state.count > this.props.max) this.reset();
-    };
-
-    render = (): ReactElement => (
-      <WrappedComponent
-        max={this.props.max}
-        count={this.state.count}
-        reset={this.reset}
-        increment={this.increment}
-      />
-    );
+  componentDidUpdate = (): void => {
+    if (this.state.count > this.props.max) this.reset();
   };
 
-const CounterComponent: VFC<Props & Partial<InjectedProps>> = ({
-  max,
-  count = 0,
-  reset = () => undefined,
-  increment = () => undefined
-}) => (
-  <div>
-    <div>
-      {count} / {max}
-    </div>
-    <button onClick={reset} type"button">
-      Reset
-    </button>
-    <button onClick={increment} type={"button"}>
-      +1
-    </button>
-  </div>
-)
+  render = (): ReactElement =>
+    this.props.children({
+      count: this.state.count,
+      reset: this.reset,
+      increment: this.increment,
+    });
+}
 
-export default withCounter(CounterComponent)
+const Counter: VFC<{ max: number }> = ({ max }) => (
+  <CounterProvider max={max}>
+    {({ count, reset, increment }) => (
+      <div>
+        <div>
+          {count} / {max}
+        </div>
+        <button onClick={reset} type="button">
+          Reset
+        </button>
+        <button onClick={increment} type="button">
+          +1
+        </button>
+      </div>
+    )}
+  </CounterProvider>
+);
+
+export default Counter;
