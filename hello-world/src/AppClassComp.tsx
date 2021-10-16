acb50de5f1c5e9e2bea626027994020dd460f511
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-16 12:52:28 +0900

    add classコンポーネントを追加
    class componentで同様のものを作成する

---
+++ hello-world/src/AppClassComp.tsx
@@ -0,0 +1,50 @@
+import { Component, ReactElement} from "react";
+import CharacterListClassComp, {Character} from "./components/CharacterListClassComp";
+import './App.css'
+
+class AppClassComp extends Component {
+  characters: Character[] = [
+    {
+      id: 1,
+      name: '桜木花道',
+      grade: 1,
+      height: 189.2,
+    },
+    {
+      id: 2,
+      name: '流川 楓',
+      grade: 1,
+      height: 187,
+    },
+    {
+      id: 3,
+      name: '宮城リョータ',
+      grade: 2,
+      height: 168,
+    },
+    {
+      id: 4,
+      name: '三井 寿',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: '赤木剛憲',
+      grade: 3,
+      height: 197,
+    },
+  ]
+
+  render(): ReactElement {
+    return (
+      <div className="container">
+        <header>
+          <h1>SLAM DUNK登場人物</h1>
+        </header>
+        <CharacterListClassComp school="湘北高校" characters={this.characters} />
+      </div>
+    );
+  }
+}
+
+export default AppClassComp
