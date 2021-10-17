74926790a32befd62ff0c86fdf6868160a02edb2
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-17 19:02:37 +0900

    add 絞り込み

--- todomvc/frontend/src/components/pages/AllTodo.tsx
+++ todomvc/frontend/src/components/pages/AllTodo.tsx
@@ -3,32 +3,49 @@ import { Box } from '@chakra-ui/react';
 import { Todo } from '../../domain/todo/Todo';
 import TodoForm from '../../containers/molecules/NewTodoForm';
 import TodoList from '../../containers/oragnisms/TodoList';
+import TodoListFooter, {
+  SelectedCategory,
+  SelectedCategoryType,
+} from '../molecules/TodoListFooter';
 
 type Props = {
   todoList: Todo[];
+  selectedCategory: SelectedCategoryType;
   add: (title: string) => void;
   completed: (id: number) => void;
   resetComplete: (id: number) => void;
   deleted: (id: number) => void;
+  changeCategory: (selectedCategory: SelectedCategoryType) => void;
 };
 
 const AllTodo: VFC<Props> = ({
   todoList = [],
+  selectedCategory = SelectedCategory.ALL,
   add = () => undefined,
   completed = () => undefined,
   resetComplete = () => undefined,
   deleted = () => undefined,
+  changeCategory = () => undefined,
 }) => (
   <>
     <Box py="5">
       <TodoForm add={add} />
     </Box>
-    <TodoList
-      todoList={todoList}
-      completed={completed}
-      resetComplete={resetComplete}
-      deleted={deleted}
-    />
+    <Box py="5">
+      <TodoList
+        todoList={todoList}
+        completed={completed}
+        resetComplete={resetComplete}
+        deleted={deleted}
+      />
+    </Box>
+    <Box>
+      <TodoListFooter
+        itemsNum={todoList.length}
+        selectedCategory={selectedCategory}
+        onChangeCategory={changeCategory}
+      />
+    </Box>
   </>
 );
 
--- todomvc/frontend/src/containers/pages/AllTodo.tsx
+++ todomvc/frontend/src/containers/pages/AllTodo.tsx
@@ -1,17 +1,42 @@
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
 import useTodos from '../../hooks/todo/todo-hooks';
 import AllTodo from '../../components/pages/AllTodo';
+import {
+  SelectedCategory,
+  SelectedCategoryType,
+} from '../../components/molecules/TodoListFooter';
+import { Todo } from '../../domain/todo/Todo';
 
 const EnhancedAllTodo: VFC = () => {
   const { todoList, add, completed, resetComplete, deleteById } = useTodos();
 
+  const [category, setCategory] = useState<SelectedCategoryType>(
+    SelectedCategory.ALL,
+  );
+
+  const changeCategory = (selectedCategory: SelectedCategoryType) => {
+    setCategory(selectedCategory);
+  };
+
+  const displayTodoList = (): Todo[] => {
+    if (category === SelectedCategory.ACTIVE)
+      return todoList.filter((todo) => todo.completedAt === null);
+
+    if (category === SelectedCategory.COMPLETE)
+      return todoList.filter((todo) => !!todo.completedAt);
+
+    return todoList;
+  };
+
   return (
     <AllTodo
-      todoList={todoList}
+      todoList={displayTodoList()}
+      selectedCategory={category}
       add={add}
       completed={completed}
       resetComplete={resetComplete}
       deleted={deleteById}
+      changeCategory={changeCategory}
     />
   );
 };
